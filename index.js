f81c3d4cbd71073aa3faa1cc2f5029a554a5e354
mazanow <mazanow.2025@gmail.com>
2026-06-29 11:49:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function resetDailyState() {
 }
 
 cron.schedule(
-  '30 13 * * 1,5',
+  '53 13 * * 1,5',
   async () => {
     //Day of week → Monday (1), Friday (5). Hour → 11 AM Minute → 52 Month (every) Day of month (every)
     try {
